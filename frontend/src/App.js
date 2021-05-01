e05e8836e6bffb0341d3ccbb97e758d05297a64f
Salman Fazal <rustamas@uni.coventry.ac.uk>
2021-04-30 21:48:11 -0400

    Full project

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core";
+import ProjectsPage from "./pages/Projects";
+import Navbar from "./components/Navbar";
+
+const useStyles = ({}) =>
+  makeStyles(() => ({
+    root: {
+      backgroundColor: "#F4F7FC",
+      minHeight: "100vh",
+    },
+  }));
+
+const App = () => {
+  const classes = useStyles({})();
+
+  return (
+    <div className={classes.root}>
+      <Navbar />
+      <ProjectsPage />
+    </div>
+  );
+};
+
+export default App;
